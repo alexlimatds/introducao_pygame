6e8872a36c173608dcd2e61f2982cc5d1fad8209
Alexandre Lima <alexlimatds@yahoo.com.br>
2025-12-22 09:20:59 -0300

    Atualizando exemplo 3

--- exemplos_básicos/exemplo_03_texto.py
+++ exemplos_básicos/exemplo_03_texto.py
@@ -8,15 +8,15 @@ clock = pygame.time.Clock()
 # Criando o objeto Font. O primeiro argumento é uma string que indica a fonte a ser utilizada.
 # O valor None faz com que o pygame use sua fonte padrão. O segundo argumento indica o tamanho da fonte.
 font = pygame.font.Font(None, 24)
+# Define o texto e cria sua superfície. O segundo argumento indica o uso ou não de anti-aliasing.
+# O terceiro argumento indica a cor do texto.
+surface_texto = font.render(f"Olá Mundo!", True, 'black')
 
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
-
-  # Define o texto e cria sua superfície. O segundo argumento indica o uso ou não de anti-aliasing.
-  # O terceiro argumento indica a cor do texto.
-  surface_texto = font.render(f"Olá Mundo!", True, 'black')
+  
   # Calcula posição do texto de forma que ele fique centralizado na janela
   x = (640 // 2) - surface_texto.get_width() // 2
   y = (480 // 2) - surface_texto.get_height() // 2
