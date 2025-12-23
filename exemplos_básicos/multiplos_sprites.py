22b32f8c5036c6c4a6bbcb23e7eacab89d12f7a7
Alexandre Lima <alexlimatds@gmail.com>
2025-12-23 20:36:16 -0300

    Exemplo de múltiplos sprites

--- exemplos_básicos/multiplos_sprites.py
+++ exemplos_básicos/multiplos_sprites.py
@@ -8,15 +8,21 @@ class Inimigo(pygame.sprite.Sprite):
   # Visualmente, é um quadrado verde que pulsa
   def __init__(self, x, y, dimensao, direcao_inicial, velocidade):
     pygame.sprite.Sprite.__init__(self)
-    self.surface = pygame.Surface([dimensao, dimensao])
-    self.rect = self.surface.get_rect()
+    self.surface_original = pygame.Surface([dimensao, dimensao])
+    self.image = self.surface_original
+    self.rect = self.surface_original.get_rect()
     self.rect.topleft = (x, y)
-    self.image = self.surface
+    # variáveis para controle de movimento
     self.direcao_inicial = direcao_inicial
     self.velocidade = velocidade
+    # variáveis para controle de pulso
+    self.dimensao_max = dimensao
+    self.dimensao_min = dimensao * 0.6
+    self.crescimento = -1
   
   def update(self):
-    self.surface.fill('green')
+    self.image.fill("green")
+    # movimentação
     if self.direcao_inicial >= 0: # subindo
       self.rect.y -= self.velocidade
       if self.rect.y < 0:
@@ -27,6 +33,14 @@ class Inimigo(pygame.sprite.Sprite):
       if self.rect.bottom > JANELA_ALTURA:
         self.rect.bottom = JANELA_ALTURA
         self.direcao_inicial = 1
+    # pulso
+    d = self.rect.width + self.crescimento
+    self.image = pygame.transform.scale(self.surface_original, (d, d))
+    self.rect = self.image.get_rect(center=self.rect.center)
+    if d >= self.dimensao_max:
+      self.crescimento = self.crescimento * -1
+    elif d <= self.dimensao_min:
+      self.crescimento = self.crescimento * -1
 
 class Personagem(pygame.sprite.Sprite):
   # Visualmente, é um quadrado azul
@@ -62,21 +76,30 @@ class Personagem(pygame.sprite.Sprite):
   def para_esquerda(self):
     # Move o personagem para a esquerda.
     self.rect.x -= self.velocidade
-    if self.rect.right < 0: # Recua o personagem de forma que ele não ultrapasse os limites da janela
-      self.rect.right = 0
+    if self.rect.x < 0: # Recua o personagem de forma que ele não ultrapasse os limites da janela
+      self.rect.x = 0
 
 # PROGRAMA PRINCIPAL
 pygame.init()
 janela = pygame.display.set_mode([JANELA_LARGURA, JANELA_ALTURA])
 clock = pygame.time.Clock()
 
-sprites = pygame.sprite.Group()
+todos_sprites = pygame.sprite.Group()
 # personagem
-personagem = Personagem(5, JANELA_ALTURA / 2, 30)
-sprites.add(personagem)
+x_personagem_inicial = 5
+personagem = Personagem(x_personagem_inicial, JANELA_ALTURA / 2, 30)
+todos_sprites.add(personagem)
 # inimigos
-inimigo = Inimigo(70, 5, 50, 1, 5)
-sprites.add(inimigo)
+inimigos = pygame.sprite.Group()
+inimigo = Inimigo(70, 5, 50, 1, 5) # inimigo 1
+todos_sprites.add(inimigo)
+inimigos.add(inimigo)
+inimigo = Inimigo(180, JANELA_ALTURA - 30, 30, -1, 20) # inimigo 2
+todos_sprites.add(inimigo)
+inimigos.add(inimigo)
+inimigo = Inimigo(270, 5, 100, 1, 2) # inimigo 3
+todos_sprites.add(inimigo)
+inimigos.add(inimigo)
 
 continuar = True
 while continuar:
@@ -96,11 +119,14 @@ while continuar:
     personagem.para_baixo()
 
   # atualização do estado do jogo
-  sprites.update() # atualiza o estado de todos os sprites
+  todos_sprites.update() # atualiza o estado de todos os sprites
+  colididos = pygame.sprite.spritecollide(personagem, inimigos, False)
+  for i in colididos:
+    personagem.rect.x = x_personagem_inicial # move personagem para posição inicial
 
   # atualização do quadro
   janela.fill((255, 255, 255))
-  sprites.draw(janela)
+  todos_sprites.draw(janela)
   pygame.display.flip()
   clock.tick(60)
 pygame.quit()
